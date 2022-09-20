165a7e8ad0e1a84dc678dc83ddec4fae1826e301
Marcelo Pereira <marcelopereiracelo0@gmail.com>
2022-09-20 12:15:47 -0300

    Estudos

---
+++ Curso_em_Video_mundo_I_II_III/III/113.1.py
@@ -0,0 +1,17 @@
+try: #operação
+    a = int(input('numerador: '))
+    b = int(input('demominador: '))
+    r = a/ b
+
+except Exception as erro: #falhou
+    print(f'Problema encontrado foi {erro.__class__}')
+except (ValueError, TypeError):
+    print('tivemos problema com o tipos de dados que vc digitou')
+except ZeroDivisionError:
+    print('não é possivel dividir um numero por zero!')
+except KeyboardInterrupt:
+    print('o usuario preferiu não informar os dados')
+else: # deu certo
+    print(f'O resultado é {r:.1f}')
+finally: # certo/falho
+    print('volte sempre! Muito obrigado!')
